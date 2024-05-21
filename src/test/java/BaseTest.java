d20c0333d6ebcad05ac6c7b1dfb47f06658ebbfc
ilkersener <ilkersener555@gmail.com>
2024-05-21 19:13:08 +0300

    Değişikliklerinizi açıklayan bir commit mesajı

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
--- src/test/java/TestPage.java
+++ src/test/java/TestPage.java
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 
 public class TestPage extends BaseTest {
 
