bd3ee8bd70e7839a556892362f1c6a834405f018
ilkersener <ilkersener555@gmail.com>
2024-05-22 01:27:46 +0300

    project complete

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -1,54 +1,81 @@
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
+// Web elementleri ve işlemleri tanımlayan Page sınıfı
 public class Page {
 
-    private WebDriver driver;
-    protected By Cherry_button = By.className("view_details");
-    private final By Orange_button = new By.ByXPath
-            ("//*[@id=\"root\"]/div/div/div/div[2]/div[2]/div/div[2]");
-    private final By Nuts_button = new By.ByXPath
-            ("//*[@id=\"root\"]/div/div/div/div[2]/div[3]/div/div[2]");
-    private final By Strawberry_button = new By.ByXPath
-            ("//*[@id=\"root\"]/div/div/div/div[2]/div[4]/div/div[2]");
+    private WebDriver driver; // WebDriver nesnesi tanımlanıyor.
+    protected By Cherry_button = By.className("view_details"); // Cherry button'a ulaşmak için locator tanımlanıyor.
+    public By Orange_button = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[2]/div[2]/div/div[2]"); // Orange button'a ulaşmak için locator tanımlanıyor.
+    public By Nuts_button = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[2]/div[3]/div/div[2]"); // Nuts button'a ulaşmak için locator tanımlanıyor.
+    public By Strawberry_button = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[2]/div[4]/div/div[2]"); // Strawberry button'a ulaşmak için locator tanımlanıyor.
+
+    // Fiyat elementlerine ulaşmak için locator'lar tanımlanıyor.
+    public By Cherry_price = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[1]/td[1]");
+    public By Orange_price = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[1]/td[2]");
+    public By Nuts_price = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[1]/td[3]");
+    public By Strawberry_price = new By.ByXPath
+            ("////*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[1]/td[4]");
+
+    // Durum elementlerine ulaşmak için locator'lar tanımlanıyor.
+    public By Cherry_condition_check = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[3]/td[1]");
+
+    public By Orange_condition_check = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[3]/td[2]");
+
+    public By Nuts_condition_check = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[3]/td[3]");
+
+    public By Strawberry_condition_check = new By.ByXPath
+            ("//*[@id=\"root\"]/div/div/div/div[3]/div/table/tbody/tr[3]/td[4]");
 
     public Page(WebDriver driver){
-        this.driver = driver;
+        this.driver = driver; // Constructor'da WebDriver nesnesi alınıyor.
     }
 
-
-
+    // Cherry button tıklanıyor ve bekleniyor.
     public void setCherry_button(String name) throws InterruptedException {
 
-        WebElement buttonSelect1 = driver.findElement(Cherry_button);
-        buttonSelect1.click();
-        WebElement button = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div[2]/div[1]/div/div[2]"));
+        WebElement cherry_selection = driver.findElement(Cherry_button);
+        cherry_selection.click();
+        WebElement button = driver.findElement(By.xpath
+                ("//*[@id=\"root\"]/div/div/div/div[2]/div[1]/div/div[2]"));
         Thread.sleep(2000);
 
     }
+
+    // Orange button tıklanıyor ve bekleniyor.
     public void setOrange_button(String name) throws InterruptedException {
 
-        WebElement buttonSelect2 = driver.findElement(Orange_button);
-        buttonSelect2.click();
+        WebElement orange_selection = driver.findElement(Orange_button);
+        orange_selection.click();
         Thread.sleep(2000);
 
     }
+
+    // Nuts button tıklanıyor ve bekleniyor.
     public void setNuts_button(String name) throws InterruptedException {
 
-        WebElement buttonSelect2 = driver.findElement(Nuts_button);
-        buttonSelect2.click();
+        WebElement nuts_selection = driver.findElement(Nuts_button);
+        nuts_selection.click();
         Thread.sleep(2000);
 
 
     }
+
+    // Strawberry button tıklanıyor.
     public void setStrawberry_button(String name){
 
-        WebElement buttonSelect2 = driver.findElement(Strawberry_button);
-        buttonSelect2.click();
+        WebElement strawberry_selection = driver.findElement(Strawberry_button);
+        strawberry_selection.click();
 
     }
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,21 +7,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseTest {
 
-    Page page = new Page(null);
-    protected WebDriver driver;
+    Page page = new Page(null); // Page sınıfından nesne oluşturuluyor.
+
+    protected WebDriver driver; // WebDriver nesnesi tanımlanıyor.
 
     @BeforeAll
     public void setUp() throws InterruptedException {
+        // WebDriver'ın ayarları yapılıyor.
         System.setProperty("webdriver.chrome,driver","drivers/chromedriver");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        // Testin başlangıcında belirtilen URL'e gidiliyor.
         driver.get("https://react-compare-app.surge.sh/");
+        // Page nesnesi oluşturuluyor.
         page = new Page(driver);
         Thread.sleep(5000);
 
-
+        // Beklenen URL ve gerçek URL karşılaştırılıyor.
+        String expectedURL = "https://react-compare-app.surge.sh/";
+        String actualURL = driver.getCurrentUrl();
 
     }
-    //@AfterAll
-    //public void systemdown(){
-       // driver.quit();
+    @AfterAll
+    public void tearDown(){
+        // WebDriver kapatılıyor.
+        if (driver!=null){
+            driver.quit();
+        }
     }
+}
